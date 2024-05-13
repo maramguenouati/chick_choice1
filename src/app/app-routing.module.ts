a7ae36989e42c11e1f415f5490342cb75683719a
maram <maramguenouatii@gmail.com>
2024-05-13 18:15:17 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
+import { AccountComponent } from './account/account.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HowItWorkComponent } from './how-it-work/how-it-work.component';
+import { HomeServiceComponent } from './home-service/home-service.component';
+import { RegisterComponent } from './register/register.component';
+import { LeftContainerComponent } from './left-container/left-container.component';
+import { RightContainerComponent } from './right-container/right-container.component';
+
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+{path:"weather",component:WeatherComponent},
+{path:"account",component:AccountComponent},
+{path:"footer",component:FooterComponent},
+{path:"navbar",component:NavbarComponent},
+{path:"howitwork",component:HowItWorkComponent},
+{path:"service",component:HomeServiceComponent},
+{path:"register",component:RegisterComponent},
+{path:"leftmeteo",component:LeftContainerComponent},
+{path:"rightmeteo",component:RightContainerComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LeftContainerComponent } from './left-container/left-container.component';
+import { RightContainerComponent } from './right-container/right-container.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { WeatherComponent } from './weather/weather.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AccountComponent } from './account/account.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeServiceComponent } from './home-service/home-service.component';
+import { HowItWorkComponent } from './how-it-work/how-it-work.component';
+import { RegisterComponent } from './register/register.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeftContainerComponent,
+    RightContainerComponent,
+    WeatherComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AccountComponent,
+    NavbarComponent,
+    HomeServiceComponent,
+    HowItWorkComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
